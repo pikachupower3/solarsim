c30e287d86679585021886a4506a25ad11afa98e
pikachupower3 <bartmm19@gmail.com>
2021-12-30 12:20:59 +0100

    3

--- solartest/Assets/Scripts/Planets/CelestialBody.cs
+++ solartest/Assets/Scripts/Planets/CelestialBody.cs
@@ -2,30 +2,28 @@
 using UnityEngine;
 
 [ExecuteInEditMode]
-[RequireComponent (typeof (Rigidbody))]
+[RequireComponent(typeof(Rigidbody))]
 public class CelestialBody : GravityObject {
 
     public float radius;
     public float surfaceGravity;
-   /* public float orbitRadius;
-    public Transform targetObject;*/
+    public float mass;
     public Vector3 initialVelocity;
     public string bodyName = "Unnamed";
     Transform meshHolder;
 
     public Vector3 velocity { get; private set; }
-    public float mass { get; private set; }
-    public double mass2 { get; private set; }
+    public float massPlanet { get; private set; }
+    public float massStar { get; private set; }
     Rigidbody rb;
 
-    void Awake () {
-        rb = GetComponent<Rigidbody> ();
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
         rb.mass = mass;
         velocity = initialVelocity;
-        /*transform.Translate(orbitRadius, 0, 0); */
     }
 
-    public void UpdateVelocity (CelestialBody[] allBodies, float timeStep) {
+    public void UpdateVelocity(CelestialBody[] allBodies, float timeStep) {
         foreach (var otherBody in allBodies) {
             if (otherBody != this) {
                 float sqrDst = (otherBody.rb.position - rb.position).sqrMagnitude;
@@ -37,19 +35,26 @@ public class CelestialBody : GravityObject {
         }
     }
 
-    public void UpdateVelocity (Vector3 acceleration, float timeStep) {
+    public void UpdateVelocity(Vector3 acceleration, float timeStep) {
         velocity += acceleration * timeStep;
     }
 
-    public void UpdatePosition (float timeStep) {
-        rb.MovePosition (rb.position + velocity * timeStep);
+    public void UpdatePosition(float timeStep) {
+        rb.MovePosition(rb.position + velocity * timeStep);
 
     }
 
-    void OnValidate () {
-        mass2 = surfaceGravity * radius * radius / Universe.gravitationalConstant / 1e+16;
-        mass = (float)mass2;
-        meshHolder = transform.GetChild (0);
+    void OnValidate() {
+        if (gameObject.name != "Sun")
+        {
+            massPlanet = (float)(surfaceGravity * radius * radius / Universe.gravitationalConstant);
+        }
+        else
+        {
+            massStar = (float)(surfaceGravity * radius * radius * 2.5 / Universe.gravitationalConstant);
+        }
+        mass = massPlanet + massStar;
+        meshHolder = transform.GetChild(0);
         meshHolder.localScale = Vector3.one * radius;
         gameObject.name = bodyName;
     }
@@ -65,13 +70,4 @@ public class CelestialBody : GravityObject {
             return rb.position;
         }
     }
-    void Update()
-    {
-        if (Input.GetKeyDown("h"))
-        {
-            Debug.Log("Mass = " + GetComponent<Rigidbody>().mass + gameObject.name);
-
-        }
-    }
-
 }
